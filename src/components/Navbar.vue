fc80e6ad1a902c69c0226ef96e556511d6640d6b
Farhana-Afrin-Maysha <farhanamaysha29@gmail.com>
2020-02-05 11:20:56 +0600

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+	<nav class="main-nav">
+		<ul>
+			<li><router-link :to="{name:'home'}">Home</router-link></li>
+			<li><router-link :to="{name:'about'}">About</router-link></li>
+			<li><router-link :to="{name:'contact'}">Contact</router-link></li>
+			
+		</ul>
+	</nav>
+	
+</div>	
+</template>
+
+<script>
+        
+
+export default{
+	name:"Navbar",
+	
+	data() {
+      return {
+    
+       }
+    }
+
+}
+</script>	
+
+<style>
+
+</style>
